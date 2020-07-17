8cfe59d1e93206ff66a0f04bebb2e881f92bc426
xingku <1351462813@qq.com>
2020-07-17 11:14:50 +0800

    伪共享

--- test/src/main/java/com/test/DayOne1.java
+++ test/src/main/java/com/test/DayOne1.java
@@ -1,7 +1,8 @@
 package com.test;
 
-public class DayOne {
-    public static volatile long[] arr = new long[2];
+
+public class DayOne1 {
+    public static volatile long[] arr = new long[8];
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
@@ -11,7 +12,7 @@ public class DayOne {
         });
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 10000_0000L; i++) {
-                arr[1] = i;
+                arr[7] = i;
             }
         });
         final long x = System.nanoTime();
@@ -23,4 +24,7 @@ public class DayOne {
         System.out.println("时间:"+(l-x)/1000_000);
     }
 
-}
+    }
+
+
+
